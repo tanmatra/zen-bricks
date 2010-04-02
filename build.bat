cc5d1bc4c8470b0f4f07c0c23f3e817f7ac7abc9
Roman Dawydkin <tanmatra@gmail.com>
2010-04-02 08:28:05 +0400

    Rename 'styles' to 'themes'.

--- build.bat
+++ build.bat
@@ -11,8 +11,8 @@ copy %PLUGINS%\org.eclipse.jface_3.6.0.*.jar %LIB%
 copy %PLUGINS%\org.eclipse.core.commands_3.5.0.*.jar %LIB%
 copy %PLUGINS%\org.eclipse.core.runtime_3.6.0.*.jar %LIB%
 copy %PLUGINS%\org.eclipse.equinox.common_3.6.0.*.jar %LIB%
-mkdir %DIST%\styles
-copy styles %DIST%\styles
+mkdir %DIST%\themes
+copy themes %DIST%\themes
 copy run.bat %DIST%
 :pack
 jar cf %LIB%\zen.jar -C build\classes zen
