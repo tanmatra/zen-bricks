d9acd2b99a43cd897a431ecab912f94a96fa522b
Roman Dawydkin <tanmatra@gmail.com>
2010-11-11 14:23:11 +0300

    XML import options dialog.

--- src/zen/bricks/ImportXMLAction.java
+++ src/zen/bricks/ImportXMLAction.java
@@ -8,8 +8,20 @@ import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
 import org.eclipse.jface.action.Action;
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.jface.window.Window;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
 import org.xml.sax.Attributes;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
@@ -21,13 +33,11 @@ class ImportXMLAction extends Action
 
     private static final class BrickHandler extends DefaultHandler
     {
-        private static final boolean GROUP_ATTRIBUTES = true;
-        private static final String ALL_ATTRS_TEXT = "@";
-        private static final String ATTR_SUFFIX = ":";
-
         private final StringBuilder buffer = new StringBuilder(128);
-
         TupleBrick brick;
+        String groupText;
+        String prefix;
+        String suffix;
 
         BrickHandler() { }
 
@@ -67,9 +77,9 @@ class ImportXMLAction extends Action
             final int attLen = attributes.getLength();
             if (attLen > 0) {
                 final TupleBrick attrParent;
-                if (GROUP_ATTRIBUTES) {
+                if (groupText != null) {
                     TupleBrick allAttrsBrick =
-                            new TupleBrick(elementBrick, ALL_ATTRS_TEXT);
+                            new TupleBrick(elementBrick, groupText);
                     allAttrsBrick.lineBreak = false;
                     attrParent = allAttrsBrick;
                 } else {
@@ -78,7 +88,7 @@ class ImportXMLAction extends Action
                 for (int i = 0; i < attLen; i++) {
                     final String attName = attributes.getQName(i);
                     final TupleBrick attNameBrick =
-                            new TupleBrick(attrParent, attName + ATTR_SUFFIX);
+                        new TupleBrick(attrParent, prefix + attName + suffix);
                     if (i == 0) {
                         attNameBrick.lineBreak = false;
                     }
@@ -121,6 +131,76 @@ class ImportXMLAction extends Action
         }
     }
 
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+    private static class ImportOptionsDialog extends Dialog
+    {
+        Button groupCheck;
+        Text attrGroupText;
+        private final BrickHandler handler;
+        private Text prefixText;
+        private Text suffixText;
+
+        protected ImportOptionsDialog(Shell parentShell, BrickHandler handler) {
+            super(parentShell);
+            this.handler = handler;
+        }
+
+        protected void configureShell(Shell newShell) {
+            super.configureShell(newShell);
+            newShell.setText("Import XML options");
+        }
+
+        protected Control createDialogArea(Composite parent) {
+            final Composite area = (Composite) super.createDialogArea(parent);
+            final GridLayout layout = (GridLayout) area.getLayout();
+            layout.numColumns = 2;
+
+            groupCheck = new Button(area, SWT.CHECK);
+            groupCheck.setText("Group attributes under single brick");
+            groupCheck.setSelection(true);
+            GridDataFactory.fillDefaults().span(2, 1).applyTo(groupCheck);
+
+            final Label attrGroupLabel = new Label(area, SWT.NONE);
+            attrGroupLabel.setText("Attribute brick text:");
+            GridDataFactory.swtDefaults().indent(20, 0).applyTo(attrGroupLabel);
+            attrGroupText = new Text(area, SWT.SINGLE | SWT.BORDER);
+            attrGroupText.setText("@");
+            GridDataFactory.swtDefaults().hint(50, SWT.DEFAULT)
+                .applyTo(attrGroupText);
+
+            groupCheck.addSelectionListener(new SelectionAdapter() {
+                public void widgetSelected(SelectionEvent e) {
+                    final boolean selection = groupCheck.getSelection();
+                    attrGroupLabel.setEnabled(selection);
+                    attrGroupText.setEnabled(selection);
+                }
+            });
+
+            new Label(area, SWT.NONE).setText("Attribute prefix:");
+            prefixText = new Text(area, SWT.SINGLE | SWT.BORDER);
+            GridDataFactory.swtDefaults().hint(50, SWT.DEFAULT)
+                .applyTo(prefixText);
+
+            new Label(area, SWT.NONE).setText("Attribute suffix:");
+            suffixText = new Text(area, SWT.SINGLE | SWT.BORDER);
+            suffixText.setText(":");
+            GridDataFactory.swtDefaults().hint(50, SWT.DEFAULT)
+                .applyTo(suffixText);
+
+            return area;
+        }
+
+        protected void okPressed() {
+            if (groupCheck.getSelection()) {
+                handler.groupText = attrGroupText.getText();
+            }
+            handler.prefix = prefixText.getText();
+            handler.suffix = suffixText.getText();
+            super.okPressed();
+        }
+    }
+
     // ================================================================== Fields
 
     private final MainWindow mainWindow;
@@ -146,10 +226,18 @@ class ImportXMLAction extends Action
         if (fileName == null) {
             return;
         }
+
+        final BrickHandler handler = new BrickHandler();
+        final ImportOptionsDialog optionsDialog =
+            new ImportOptionsDialog(mainWindow.getShell(), handler);
+        if (optionsDialog.open() != Window.OK) {
+            return;
+        }
+
         path = new File(fileName).getParent();
         try {
             final Editor editor = mainWindow.editor;
-            final TupleBrick root = parse(fileName);
+            final TupleBrick root = parse(fileName, handler);
             editor.setRoot(root);
             mainWindow.setTitle(fileName);
         } catch (Exception e) {
@@ -157,11 +245,12 @@ class ImportXMLAction extends Action
         }
     }
 
-    private TupleBrick parse(String fileName) throws Exception {
+    private TupleBrick parse(String fileName, BrickHandler handler)
+        throws Exception
+    {
         final SAXParserFactory saxFactory = SAXParserFactory.newInstance();
         saxFactory.setValidating(false);
         final SAXParser saxParser = saxFactory.newSAXParser();
-        final BrickHandler handler = new BrickHandler();
         saxParser.parse(new File(fileName), handler);
         return handler.brick;
     }
